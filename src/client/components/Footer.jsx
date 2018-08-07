5103d1902e6c7c6313537b8c1be7c8f40dd59d26
saintfirewater <hwasoosung@cedar.kr>
2018-08-08 00:02:33 +0900

    Complete Footer component
    Because the images of logo and copyright are too close each other, fix
    the value of y in logo.svg.

---
+++ src/client/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../../../public/css/footer.css';
+
+function Footer() {
+  return (
+    <div id="footer">
+      <img 
+        id="footer_logo_and_copyright" 
+        src="/img/logo.svg" 
+        alt="logo and copyright"
+      />
+    </div>
+  );
+}
+
+export default Footer;
